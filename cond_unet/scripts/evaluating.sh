96704dcd0bdc734b535f180b05c24e8d74797a3d
Shutian-Liang <1479775170@qq.com>
2025-05-07 14:51:56 +0800

    test

--- cond_unet/scripts/evaluating.sh
+++ cond_unet/scripts/evaluating.sh
@@ -11,8 +11,15 @@
 #     --sampling_noise gaussian --history 1 > logfiles/eval/gaussian_gs_seen.log 2>&1 )& \
 
 ### here is the no seen here
-(nohup python -u evaluating.py --device 3 --training_noise gaussian \
-    --sampling_noise ou --history 0 > logfiles/eval/gaussian_os_unseen.log 2>&1 )& \
+# (nohup python -u evaluating.py --device 3 --training_noise gaussian \
+#     --sampling_noise ou --history 0 > logfiles/eval/gaussian_os_unseen.log 2>&1 )& \
 
-(nohup python -u evaluating.py --device 2 --training_noise gaussian \
-    --sampling_noise gaussian --history 0 > logfiles/eval/gaussian_gs_unseen.log 2>&1 )& \
+# (nohup python -u evaluating.py --device 2 --training_noise gaussian \
+#     --sampling_noise gaussian --history 0 > logfiles/eval/gaussian_gs_unseen.log 2>&1 )& \
+
+# ou training noise and gaussian sampling noise
+(nohup python -u evaluating.py --device 3 --training_noise ou \
+    --sampling_noise gaussian --history 0 > logfiles/eval/ou_gs_unseen.log 2>&1 )& \
+
+(nohup python -u evaluating.py --device 2 --training_noise ou \
+    --sampling_noise gaussian --history 1 > logfiles/eval/ou_gs_seen.log 2>&1 )& \
